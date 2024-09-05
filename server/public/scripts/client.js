a9cf475796e48ce15a500279ef90bcbd7bec4d6b
padifa <padifa7@live.fr>
2024-09-05 11:44:37 -0500

    created a form to add artist

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,53 +1,60 @@
 function onReady() {
-    console.log('Hello from client.js');
+  console.log("Hello from client.js");
 
-    axios({
-        method: 'GET',
-        url: '/artist'
-    })
-        .then(function (response) {
-            // Code that will run on successful response
-            // from the server.
-            console.log(response);
-            // quotesFromServer will be an Array of quotes
-            let quotesFromServer = response.data;
-            let contentDiv = document.querySelector('#artistTableBody');
-            for (let artist of quotesFromServer) {
-                contentDiv.innerHTML += `
+  axios({
+    method: "GET",
+    url: "/artist",
+  })
+    .then(function (response) {
+      // Code that will run on successful response
+      // from the server.
+      console.log(response);
+      // quotesFromServer will be an Array of quotes
+      let quotesFromServer = response.data;
+      let contentDiv = document.querySelector("#artistTableBody");
+      for (let artist of quotesFromServer) {
+        contentDiv.innerHTML += `
                 <tr>
                     <td>${artist.name}</td>
                     <td>${artist.born}</td>
                     <td>${artist.died}</td>
                 </tr>
             `;
-            }
-        }).catch(function (error) {
-            // Code that will run on any errors from the server.
-            console.log(error);
-            alert('Something bad happened! Check the console for more details.')
-        });
-axios({
-    method: 'GET',
-    url: '/song'  
-})
-.then(function (response) {
-    console.log(response);
-    let songFromServer = response.data;
-    let contentDiv = document.querySelector('#songTableBody');
-    for (let song of songFromServer) {
+      }
+    })
+    .catch(function (error) {
+      // Code that will run on any errors from the server.
+      console.log(error);
+      alert("Something bad happened! Check the console for more details.");
+    });
+  axios({
+    method: "GET",
+    url: "/song",
+  })
+    .then(function (response) {
+      console.log(response);
+      let songFromServer = response.data;
+      let contentDiv = document.querySelector("#songTableBody");
+      for (let song of songFromServer) {
         contentDiv.innerHTML += `
         <tr>
         <td>${song.title}</td>
         <td>${song.artist}</td>
         </tr>
         `;
-    }s
-}).catch(function (error) {
-    console.log(error);
-    alert('something bad had happened! Check the console for more details')
-});
-        
-    // TODO Add Axios request for /songs and display on DOM
-}
+      }
+      s;
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert("something bad had happened! Check the console for more details");
+    });
 
+  // TODO Add Axios request for /songs and display on DOM
+}
+function addArtist(event) {
+  event.preventDefault();
+
+  console.log("add artist");
+}
 onReady();
