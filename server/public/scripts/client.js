e43d590bced9b15baf28b27c051d1a75b69f798a
padifa <padifa7@live.fr>
2024-09-05 22:11:56 -0500

    song form added , added GET and POST for song

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,61 +1,110 @@
 function onReady() {
   console.log("Hello from client.js");
+  fetchArtist();
 
+  fetchSongs();
+  // TODO Add Axios request for /songs and display on DOM
+}
+
+function addArtist(event) {
+  //prevents page from reloading...
+  event.preventDefault();
+
+  //getting user provided data from the DOM
+  const artistName = document.getElementById("artist-name").value;
+  const artistBorn = document.getElementById("artist-born").value;
+  const artistDied = document.getElementById("artist-died").value;
+
+  //this is our newArtist object that will be sent to the server via a POST method
+  let newArtist = {
+    name: artistName,
+    born: artistBorn,
+    died: artistDied,
+  };
+
+  console.log("add artist", newArtist);
+  axios({
+    method: "POST",
+    url: "/artist",
+    data: newArtist,
+  })
+    .then((response) => {
+      fetchArtist();
+
+      document.getElementById("artist-name").value = "";
+      document.getElementById("artist-born").value = "";
+      document.getElementById("artist-died").value = "";
+    })
+    .catch((err) => console.error(`Error Artist`, err));
+}
+
+function addSong(event) {
+  event.preventDefault();
+  const songTitle = document.getElementById("song-title").value;
+  const songArtist = document.getElementById("song-artist").value;
+
+  let newSong = {
+    title: songTitle,
+    artist: songArtist,
+  };
+  console.log("add song", newSong);
+  axios({
+    method: "POST",
+    url: "/song",
+    data: newSong,
+  })
+    .then((response) => {
+      fetchSongs();
+
+      document.getElementById("song-title").value = "";
+      document.getElementById("song-artist").value = "";
+    })
+    .catch((err) => console.error(`Error Song`, err));
+}
+function fetchArtist() {
   axios({
     method: "GET",
     url: "/artist",
   })
     .then(function (response) {
-      // Code that will run on successful response
-      // from the server.
-      console.log(response);
-      // quotesFromServer will be an Array of quotes
-      let quotesFromServer = response.data;
-      let contentDiv = document.querySelector("#artistTableBody");
-      for (let artist of quotesFromServer) {
-        contentDiv.innerHTML += `
-                <tr>
-                    <td>${artist.name}</td>
-                    <td>${artist.born}</td>
-                    <td>${artist.died}</td>
-                </tr>
-            `;
+      console.log(`we received something from the server`, response);
+      const artistData = response.data;
+      document.getElementById("artistTableBody").innerHTML = ``;
+      for (const artist of artistData) {
+        document.getElementById("artistTableBody").innerHTML += `
+      <tr>
+      <td>${artist.name}</td>
+      <td>${artist.born}</td>
+      <td>${artist.died}</td>
+      </tr>
+      `;
       }
     })
     .catch(function (error) {
-      // Code that will run on any errors from the server.
-      console.log(error);
-      alert("Something bad happened! Check the console for more details.");
+      console.error(`There was an error fetching artists`, error);
     });
-
+}
+function fetchSongs() {
   axios({
     method: "GET",
     url: "/song",
   })
     .then(function (response) {
-      console.log(response);
-      let songFromServer = response.data;
-      let contentDiv = document.querySelector("#songTableBody");
-      for (let song of songFromServer) {
-        contentDiv.innerHTML += `
+      console.log(`we received something from the server`, response);
+      const songData = response.data;
+      document.getElementById("songTableBody").innerHTML = ``;
+      for (const song of songData) {
+        document.getElementById("songTableBody").innerHTML += `
         <tr>
-        <td>${song.title}</td>
         <td>${song.artist}</td>
-        </tr>
-        `;
+        <td>${song.title}</td>
+        </tr>`;
       }
-      ;
     })
     .catch(function (error) {
-      console.log(error);
-      alert("something bad had happened! Check the console for more details");
+      console.error(`There is an error fetching songs`, error);
     });
-
-  // TODO Add Axios request for /songs and display on DOM
 }
-function addArtist(event) {
-  event.preventDefault();
 
-  console.log("add artist");
-}
+//this function runs when index.html loads...
 onReady();
--- server/server.js
+++ server/server.js
@@ -1,75 +1,90 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = 5001;
 
 const artistListArray = [
-    {
-        name: 'Miles Davis',
-        born: 1926,
-        died: 1990,
-    },
-    {
-        name: 'Duke Ellington',
-        born: 1899,
-        died: 1974,
-    },
-    {
-        name: 'John Coltrane',
-        born: 1926,
-        died: 1987,
-    },
-    {
-        name: 'Louis Daniel Armstrong',
-        born: 1901,
-        died: 1971,
-    },
+  {
+    name: "Miles Davis",
+    born: 1926,
+    died: 1990,
+  },
+  {
+    name: "Duke Ellington",
+    born: 1899,
+    died: 1974,
+  },
+  {
+    name: "John Coltrane",
+    born: 1926,
+    died: 1987,
+  },
+  {
+    name: "Louis Daniel Armstrong",
+    born: 1901,
+    died: 1971,
+  },
 ];
 
 const songListArray = [
-    {
-        title: 'Take Five',
-        artist: 'The Dave Brubeck Quartet',
-    },
-    {
-        title: 'So What',
-        artist: 'Miles Davis',
-    },
-    {
-        title: 'Sing Sing Sing',
-        artist: 'Benny Goodman',
-    },
-    {
-        title: 'Take the "A" Train',
-        artist: 'The Dave Brubeck Quartet',
-    },
+  {
+    title: "Take Five",
+    artist: "The Dave Brubeck Quartet",
+  },
+  {
+    title: "So What",
+    artist: "Miles Davis",
+  },
+  {
+    title: "Sing Sing Sing",
+    artist: "Benny Goodman",
+  },
+  {
+    title: 'Take the "A" Train',
+    artist: "The Dave Brubeck Quartet",
+  },
 ];
 
 //the app.use is express middleware
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 // this line here is ONLY needed for handling POST requests (not GET)
 app.use(express.json()); // turns on req.body for POST requests
 
-app.get('/artist', (req, res) => {
-    res.send(artistListArray);
+app.get("/artist", (req, res) => {
+  res.send(artistListArray);
 });
 
 // TODO - Add GET for songs
-app.get('/song', (req,res) => {
-    console.log('Get/ song is being handled');
-    res.send(songListArray);
-})
+app.get("/song", (req, res) => {
+  console.log("Get/ song is being handled");
+  res.send(songListArray);
+});
 
-app.post('/artist', (req, res) => {
-    console.log(req.body);
-if (req.body.name && req.body.born && req.body.died) {
-        artistListArray.push(req.body);
-        res.sendStatus(201);
-    } else {
-        res.status(400).send('Oops! Quote objects should have `name` and `born` and `died` keys');
-    }
-})
+app.post("/artist", (req, res) => {
+  console.log(req.body);
+  if (req.body.name && req.body.born && req.body.died) {
+    artistListArray.push(req.body);
+    res.sendStatus(201);
+  } else {
+    res
+      .status(400)
+      .send(
+        "Oops! Artist objects should have `name` and `born` and `died` keys"
+      );
+  }
+});
+app.post("/song", (req, res) => {
+  console.log(req.body);
+  if (req.body.title && req.body.artist) {
+    songListArray.push(req.body);
+    res.sendStatus(201);
+  } else {
+    res
+      .status(400)
+      .send("Oops! Song's objects should have `title` and `artist` keys");
+  }
+});
 
 app.listen(PORT, () => {
-    console.log('listening on port', PORT)
+  console.log("listening on port", PORT);
 });
